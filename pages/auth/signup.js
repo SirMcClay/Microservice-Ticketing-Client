986181e46ff51401bad396219cadda16ef768818
sirmcclay <mcclay.design@gmail.com>
2020-12-16 09:43:17 -0300

    Handle form fields, form submit  and parsist on states

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+
 const signup = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		console.log(email, password);
+	};
+
 	return (
-		<form>
+		<form onSubmit={onSubmit}>
 			<h1>Sign Up</h1>
 			<div className="form-group">
 				<label htmlFor="email">Email Address</label>
-				<input type="text" name="email" className="form-control" />
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="text"
+					name="email"
+					className="form-control"
+				/>
 			</div>
 			<div className="form-group">
 				<label htmlFor="password">Passoword</label>
-				<input type="password" name="password" className="form-control" />
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					name="password"
+					className="form-control"
+				/>
 			</div>
 			<button className="btn btn-primary">Sign Up</button>
 		</form>
