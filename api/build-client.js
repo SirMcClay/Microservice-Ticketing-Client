dd6f8cdcfaebef8df4042a1eb787234cc6c09126
sirmcclay <mcclay.design@gmail.com>
2020-12-30 13:39:30 -0300

    Ingress controller local baseURL corrected

--- api/build-client.js
+++ api/build-client.js
@@ -5,8 +5,7 @@ const buildClient = ({ req }) => {
 		// We are on the server
 
 		return axios.create({
-			baseURL:
-				'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+			baseURL: 'http://ingress-nginx-controller.kube-system.svc.cluster.local',
 			headers: req.headers,
 		});
 	} else {
