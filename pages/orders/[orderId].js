f9dabe257106f6efa033d62e784518e39434ff04
sirmcclay <mcclay.design@gmail.com>
2021-01-20 12:40:11 -0300

    Get orderId inside order page and fetch order data - plus calculate time left to order expire

--- pages/orders/[orderId].js
+++ pages/orders/[orderId].js
@@ -1,5 +1,14 @@
-const OrderShow = () => {
-	return <div>OrderShow</div>;
+const OrderShow = ({ order }) => {
+	const msLeft = new Date(order.expiresAt) - new Date();
+
+	return <div>{msLeft / 1000} seconds until order expires</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+	const { orderId } = context.query;
+	const { data } = await client.get(`/api/orders/${orderId}`);
+
+	return { order: data };
 };
 
 export default OrderShow;
