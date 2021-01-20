df82c1058765f3d712b468b0162209dbed3da529
sirmcclay <mcclay.design@gmail.com>
2021-01-20 12:54:38 -0300

    Refactor time to expire calculation every 1 second with useState, useEffect and setInterval

--- pages/orders/[orderId].js
+++ pages/orders/[orderId].js
@@ -1,7 +1,23 @@
-const OrderShow = ({ order }) => {
-	const msLeft = new Date(order.expiresAt) - new Date();
+import { useEffect, useState } from 'react';
 
-	return <div>{msLeft / 1000} seconds until order expires</div>;
+const OrderShow = ({ order }) => {
+	const [timeLeft, setTimeLeft] = useState('');
+
+	useEffect(() => {
+		const findTimeLeft = () => {
+			const msLeft = new Date(order.expiresAt) - new Date();
+			setTimeLeft(Math.round(msLeft / 1000));
+		};
+
+		findTimeLeft();
+		const timerId = setInterval(findTimeLeft, 1000);
+
+		return () => {
+			clearInterval(timerId);
+		};
+	}, [order]);
+
+	return <div>Time left to pay: {timeLeft} seconds</div>;
 };
 
 OrderShow.getInitialProps = async (context, client) => {
